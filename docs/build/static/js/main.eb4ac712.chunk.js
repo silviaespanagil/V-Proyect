b12b06ab9a3fb7936f5290d767a784f037efa6c1
NurAhissami <nurahi10@gmail.com>
2021-06-07 13:24:25 +0200

    Add work in progress gif

---
+++ docs/build/static/js/main.eb4ac712.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-react-project"]=this["webpackJsonpmy-react-project"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),o=c.n(s),a=(c(8),c(9),c(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("div",{className:"progress"})})};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.eb4ac712.chunk.js.map
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import '../stylesheets/App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="progress"></div>
+    </div>
+  );
 }
 export default App;
