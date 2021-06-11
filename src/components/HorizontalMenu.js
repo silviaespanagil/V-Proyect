d25db725f3a074fd393eda673df4f38712b78f80
silviaespanagil <silviaespanagil@gmail.com>
2021-06-11 19:50:28 +0200

    Adjust styles for desktop breakpoint

--- src/components/HorizontalMenu.js
+++ src/components/HorizontalMenu.js
@@ -3,7 +3,7 @@ import noLogo from "../images/giphypig.gif";
 const HorizontalMenu = () => {
   return (
     <div className="hMenu">
-      <img className="hMenu__img" src={noLogo} />
+      <img className="hMenu__img" src={noLogo} alt="logo" />
       <ul className="hMenu__ul">
         <li className="hMenu__item">Sobre nosotras</li>
         <li className="hMenu__item">Mis favoritos</li>
